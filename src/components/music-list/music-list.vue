6880afc25864094b9e03752a7624b12af6bd95dc
fun <yangf325@163.com>
2019-01-29 15:52:28 +0800

    歌手详情全部开发

--- src/components/music-list/music-list.vue
+++ src/components/music-list/music-list.vue
@@ -1,11 +1,17 @@
 <template>
   <div class="music-list">
-    <div class="back">
+    <div class="back" @click="back">
       <i class="icon-back"></i>
     </div>
     <h1 class="title" v-html="title"></h1>
     <div class="bg-image" :style="bgStyle" ref="bgImage">
-      <div class="filter"></div>
+      <div class="play-wrapper" ref="playBtn">
+        <div class="play" v-show="songs.length>0">
+          <i class="icon-play"></i>
+          <span class="text">随机播放全部</span>
+        </div>
+      </div>
+      <div class="filter" ref="filter"></div>
     </div>
     <div class="bg-layer" ref="layer"></div>
     <scroll
@@ -19,12 +25,22 @@
       <div class="song-list-wrapper">
         <song-list :songs="songs"></song-list>
       </div>
+      <div class="loading-container" v-show="!songs.length">
+        <loading></loading>
+      </div>
     </scroll>
   </div>
 </template>
 <script>
 import SongList from "base/song-list/song-list";
 import Scroll from "base/scroll/scroll";
+import Loading from "base/loading/loading"
+import { prefixStyle } from "common/js/dom";
+
+const RESERVED_HEIGHT = 40;
+const backdrop = prefixStyle("backdrop-filter");
+const transform = prefixStyle("transform");
+
 export default {
   props: {
     bgImage: {
@@ -52,14 +68,42 @@ export default {
   },
   watch: {
     scrollY(newY) {
+      let zIndex = 0;
+      let scale = 1;
+      let blur = 0;
       let translateY = Math.max(-this.minTranslateY, newY);
-      this.$refs.layer.style["transform"] = `translate3d(0,${translateY}px,0)`;
-      this.$refs.layer.style["webkit-transform"] = `translate3d(0,${translateY}px,0)`;
+      this.$refs.layer.style[transform] = `translate3d(0,${translateY}px,0)`;
+      const percent = Math.abs(newY / -this.imageHeight);
+      //下拉上升的图片缩放
+      if (newY > 0) {
+        scale = 1 + percent;
+        zIndex = 10;
+      } else {
+        // scale = 1 - percent;
+        blur = Math.min(20 * percent, 20);
+      }
+      this.$refs.filter.style[backdrop] = `blur(${blur}px)`;
+      //背景跟随滚动上升以及停止
+      if (newY < -this.minTranslateY + RESERVED_HEIGHT) {
+        zIndex = 10;
+        this.$refs.bgImage.style.paddingTop = 0;
+        this.$refs.bgImage.style.height = `${RESERVED_HEIGHT}px`;
+        this.$refs.playBtn.style.display = "none";
+      } else {
+        this.$refs.bgImage.style.paddingTop = "70%";
+        this.$refs.playBtn.style.display = "";
+        this.$refs.bgImage.style.height = 0;
+      }
+      this.$refs.bgImage.style.zIndex = zIndex;
+      this.$refs.bgImage.style[transform] = `scale(${scale})`;
     }
   },
   methods: {
     scroll(pos) {
       this.scrollY = pos.y;
+    },
+    back() {
+      this.$router.back();
     }
   },
   mounted() {
@@ -73,7 +117,8 @@ export default {
   },
   components: {
     Scroll,
-    SongList
+    SongList,
+    Loading
   }
 };
 </script>
